c3dfe0d874adfe1b050fb00cd680a9df0f3afbfc
whj11 <whj11@DESKTOP-HBIBQBV>
2017-07-17 16:39:08 +0900

    up

--- 170717/src/ipodo/wjm/main/Main.java
+++ 170717/src/ipodo/wjm/main/Main.java
@@ -1,7 +1,5 @@
 package ipodo.wjm.main;
 
-import java.util.List;
-
 import ipodo.wjm.external.ValueOf;
 import ipodo.wjm.internal.impl.ConversionServiceImpl;
 
@@ -12,15 +10,14 @@ public class Main {
 		ConversionServiceImpl csi = new ConversionServiceImpl();
 
 		ValueOf vo = new ValueOf();
+		
 		// 문자열 list
-
 		csi.setExternalSpell(vo);
+		
 		csi.setSpelling("wonjungmin");
 
 		// 소수점 2자리
-
-		csi.setExternalNum(vo.setNum(123.36456456456));
-
+		csi.setExternalNum(vo.setNum(123.36656456456));
 
 	}
 
