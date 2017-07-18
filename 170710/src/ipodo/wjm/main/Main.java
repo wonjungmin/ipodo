39de94af26b6f9348bf5e1b63ba16f06682ec409
whj11 <whj11@DESKTOP-HBIBQBV>
2017-07-18 11:40:33 +0900

    1717

--- 170710/src/ipodo/wjm/main/Main.java
+++ 170710/src/ipodo/wjm/main/Main.java
@@ -1,5 +1,7 @@
 package ipodo.wjm.main;
 
+import java.util.Scanner;
+
 import ipodo.wjm.serviceImpl.ClassA;
 import ipodo.wjm.serviceImpl.ClassB;
 import ipodo.wjm.serviceImpl.WriteStringFuncImpl;
@@ -7,16 +9,28 @@ import ipodo.wjm.serviceImpl.WriteStringFuncImpl;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		WriteStringFuncImpl wsf = new WriteStringFuncImpl();
-		
-		wsf.setString(new ClassA());
-		wsf.writeString();
-		
-		wsf.setString(new ClassB());
-		wsf.writeString();
 
+		WriteStringFuncImpl wsf = new WriteStringFuncImpl();
+
+		String message;
+		Scanner scan = new Scanner(System.in); // 문자 입력을 인자로 Scanner 생성
+
+		System.out.println("메시지 a나 b를 입력하세요:");
 		
+		message = scan.nextLine();
+		
+		if (message.equals("a")) {
+			
+			
+			wsf.setString(new ClassA());
+			wsf.writeString();
+		}else {
+			
+			wsf.setString(new ClassB());
+			wsf.writeString();
+		}
+
+
 	}
 
 }
